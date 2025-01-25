b73486053241bf2e7d8ee170c6dbba4533f355e5
peta-skr <atu.cho.33@gmail.com>
2025-01-25 22:09:48 +0900

    20250125

--- Chapter7/main.go
+++ Chapter7/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 
 	codewriter "github.com/peta-skr/NAND2Tetris/Chapter7/codeWriter"
@@ -12,8 +13,8 @@ import (
 var Stack = make([]int, 0)
 
 func main() {
-	parseData, err := parser.Constructor("./test/SimpleAdd/SimpleAdd.vm")
-	output := codewriter.Constructor("./test/SimpleAdd/SimpleAdd.asm")
+	parseData, err := parser.Constructor("./test/StackTest/StackTest.vm")
+	output := codewriter.Constructor("./test/StackTest/StackTest.asm")
 
 	if err != nil {
 		fmt.Println("some Error")
@@ -27,12 +28,8 @@ func main() {
 		case parser.C_ARITHMETIC:
 			command := parseData.Arg1()
 			if command == "add" {
-				// num1 := Stack[len(Stack) - 1]
-				// num2 := Stack[len(Stack) - 2]
 				Stack = Stack[:len(Stack) - 2]
 
-				// str := "Add " + strconv.Itoa(num1) + " " + strconv.Itoa(num2)
-				
 				output.WriteArithmetic("@SP")
 				output.WriteArithmetic("M=M-1")
 				output.WriteArithmetic("A=M")
@@ -43,6 +40,47 @@ func main() {
 				output.WriteArithmetic("M=D+M")
 				output.WriteArithmetic("@SP")
 				output.WriteArithmetic("M=M+1")
+			}else if command == "eq" {
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
+				Stack = Stack[:len(Stack) - 2]
+
+				if s1 == s2 {
+					Stack = append(Stack, 0)
+				}else {
+					Stack = append(Stack, -1)
+				}
+
+				label1 := RandomString()
+				label2 := RandomString()
+
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M-D")
+				output.WriteArithmetic("@"+label1)
+				output.WriteArithmetic("D;JEQ")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=0")
+				output.WriteArithmetic("@"+label2)
+				output.WriteArithmetic("0;JMP")
+				output.WriteArithmetic("(" + label1 + ")")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=-1")
+				output.WriteArithmetic("(" + label2 + ")")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+
+
 			}
 		case parser.C_PUSH:
 			str := parseData.Arg2()
@@ -65,4 +103,18 @@ func main() {
 			output.WriteArithmetic("M=M+1")
 		}
 	}
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+// https://qiita.com/srtkkou/items/ccbddc881d6f3549baf1
+func RandomString() string {
+
+	// r := rand.New(time.Now().UnixNano())
+
+	b := make([]rune, 20)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
 }
