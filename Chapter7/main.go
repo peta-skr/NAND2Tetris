47619e73b4f38d07c6d781b1b86bea46363b74b9
peta-skr <atu.cho.33@gmail.com>
2025-01-26 10:51:22 +0900

    complete to implement stack arithmetic command in chapter7

--- Chapter7/main.go
+++ Chapter7/main.go
@@ -27,8 +27,13 @@ func main() {
 		switch parseData.CommandType() {
 		case parser.C_ARITHMETIC:
 			command := parseData.Arg1()
-			if command == "add" {
+			switch command {
+			case "add":
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
 				Stack = Stack[:len(Stack) - 2]
+
+				Stack = append(Stack, s1 + s2)
 
 				output.WriteArithmetic("@SP")
 				output.WriteArithmetic("M=M-1")
@@ -40,7 +45,25 @@ func main() {
 				output.WriteArithmetic("M=D+M")
 				output.WriteArithmetic("@SP")
 				output.WriteArithmetic("M=M+1")
-			}else if command == "eq" {
+			case "sub":
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
+				Stack = Stack[:len(Stack) - 2]
+
+				Stack = append(Stack, s1 - s2)
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=M-D")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+				
+			case "eq":
 				s1 := Stack[len(Stack) - 1]
 				s2 := Stack[len(Stack) - 2]
 				Stack = Stack[:len(Stack) - 2]
@@ -79,8 +102,142 @@ func main() {
 				output.WriteArithmetic("(" + label2 + ")")
 				output.WriteArithmetic("@SP")
 				output.WriteArithmetic("M=M+1")
+			case "lt":
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
+				Stack = Stack[:len(Stack) - 2]
+
+				if s2 < s1 {
+					Stack = append(Stack, 0)
+				}else {
+					Stack = append(Stack, -1)
+				}
+
+				label1 := RandomString()
+				label2 := RandomString()
 
 
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M-D")
+				output.WriteArithmetic("@"+label1)
+				output.WriteArithmetic("D;JLT")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=0")
+				output.WriteArithmetic("@"+label2)
+				output.WriteArithmetic("0;JMP")
+				output.WriteArithmetic("(" + label1 + ")")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=-1")
+				output.WriteArithmetic("(" + label2 + ")")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+			case "gt":
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
+				Stack = Stack[:len(Stack) - 2]
+
+				if s2 > s1 {
+					Stack = append(Stack, 0)
+				}else {
+					Stack = append(Stack, -1)
+				}
+
+				label1 := RandomString()
+				label2 := RandomString()
+
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M-D")
+				output.WriteArithmetic("@"+label1)
+				output.WriteArithmetic("D;JGT")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=0")
+				output.WriteArithmetic("@"+label2)
+				output.WriteArithmetic("0;JMP")
+				output.WriteArithmetic("(" + label1 + ")")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=-1")
+				output.WriteArithmetic("(" + label2 + ")")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+			case "neg":
+				s1 := Stack[len(Stack) - 1]
+				Stack = Stack[:len(Stack) - 1]
+
+				Stack = append(Stack, -s1)
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=-M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+			case "and":
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
+				Stack = Stack[:len(Stack) - 2]
+
+				Stack = append(Stack, s1 & s2)
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=D&M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+			case "or":
+				s1 := Stack[len(Stack) - 1]
+				s2 := Stack[len(Stack) - 2]
+				Stack = Stack[:len(Stack) - 2]
+
+				Stack = append(Stack, s1 | s2)
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("D=M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=D|M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
+			case "not":
+				s1 := Stack[len(Stack) - 1]
+				Stack = Stack[:len(Stack) - 1]
+
+				Stack = append(Stack, ^s1)
+
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M-1")
+				output.WriteArithmetic("A=M")
+				output.WriteArithmetic("M=!M")
+				output.WriteArithmetic("@SP")
+				output.WriteArithmetic("M=M+1")
 			}
 		case parser.C_PUSH:
 			str := parseData.Arg2()
