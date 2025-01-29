8d1b3a737e2d30887393a1dca968b3a23bdd8ce5
peta-skr <atu.cho.33@gmail.com>
2025-01-29 21:14:05 +0900

    20250129

--- Chapter7/main.go
+++ Chapter7/main.go
@@ -269,37 +269,36 @@ func main() {
 				fmt.Println("some Error")
 				return
 			}
-			str1 := "@" + strconv.Itoa(len(Stack) + 256)
-			output.WriteArithmetic(str1)
 			Stack = append(Stack, num)
-			output.WriteArithmetic("D=A")
-			output.WriteArithmetic("@SP")
-			output.WriteArithmetic("M=D")
 			output.WriteArithmetic("@"+str)
 			output.WriteArithmetic("D=A")
-			output.WriteArithmetic(str1)
+			output.WriteArithmetic("@SP")
+			output.WriteArithmetic("A=M")
 			output.WriteArithmetic("M=D")
 			output.WriteArithmetic("@SP")
 			output.WriteArithmetic("M=M+1")
 		case "local":
 			str := parseData.Arg2()
-			num, err := strconv.Atoi(str)
-		if err != nil {
-			fmt.Println("some Error")
-			return
-		}
-		str1 := "@" + strconv.Itoa(len(Stack) + 256)
-		output.WriteArithmetic(str1)
-		Stack = append(Stack, num)
-		output.WriteArithmetic("D=A")
+		// 	num, err := strconv.Atoi(str)
+		// if err != nil {
+		// 	fmt.Println("some Error")
+		// 	return
+		// }
+		output.WriteArithmetic("@LCL")
+		output.WriteArithmetic("D=M")
+		output.WriteArithmetic("@" + str)
+		output.WriteArithmetic("D=D+A")
+		output.WriteArithmetic("A=D")
+		output.WriteArithmetic("D=M")
 		output.WriteArithmetic("@SP")
+		output.WriteArithmetic("A=M")
 		output.WriteArithmetic("M=D")
-		output.WriteArithmetic("@"+str)
-		output.WriteArithmetic("D=A")
-		output.WriteArithmetic(str1)
-		output.WriteArithmetic("M=D")
-		output.WriteArithmetic("@SP")
-		output.WriteArithmetic("M=M+1")
+		output.WriteArithmetic("@LCL")
+		output.WriteArithmetic("D=M")
+		output.WriteArithmetic("@" + str)
+		output.WriteArithmetic("D=D+A")
+		output.WriteArithmetic("A=D")
+		output.WriteArithmetic("M=0")
 		}
 		case parser.C_POP:
 			str := parseData.Arg2()
